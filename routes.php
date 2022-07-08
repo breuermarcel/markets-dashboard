0d345d4b1c78cfb8c12924e8e863e40ed8935b0c
Marcel Breuer <breuer.marcel@outlook.com>
2022-07-08 12:47:12 +0200

    middleware "web" added

--- routes.php
+++ routes.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('finance-dashboard')->group(function () {
+Route::prefix('finance-dashboard')->middleware("web")->group(function () {
     Route::prefix('stocks')->group(function () {
-
+        
     });
 
     Route::prefix('analysis')->group(function () {
