daa708e123ce474113aff985fc59aeec1a8ae2e4
Marcel Breuer <breuer.marcel@outlook.com>
2022-07-08 13:00:49 +0200

    load Views

--- src/FinanceDashboardServiceProvider.php
+++ src/FinanceDashboardServiceProvider.php
@@ -15,7 +15,7 @@ class FinanceDashboardServiceProvider extends ServiceProvider
          * Optional methods to load your package assets
          */
         // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'finance-dashboard');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'finance-dashboard');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'finance-dashboard');
         // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         $this->loadRoutesFrom(__DIR__.'/routes.php');
 
