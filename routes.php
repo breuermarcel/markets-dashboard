0b03275c97f97e28d48609f9ca2dc9792d7875b5
Marcel Breuer <breuer.marcel@outlook.com>
2022-07-08 12:59:22 +0200

    Stocks added

--- routes.php
+++ routes.php
@@ -1,11 +1,10 @@
 <?php
 
+use Breuermarcel\FinanceDashboard\Http\Controllers\StockController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('finance-dashboard')->middleware("web")->group(function () {
-    Route::prefix('stocks')->group(function () {
-        
-    });
+    Route::resource('stocks', StockController::class);
 
     Route::prefix('analysis')->group(function () {
 
---
+++ src/Models/Stock.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Breuermarcel\FinanceDashboard\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Stock extends Model
+{
+    use HasFactory;
+}
