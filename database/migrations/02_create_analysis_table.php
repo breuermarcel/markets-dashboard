b25340ff9228a18e7540868899f04268eef0cf26
Marcel Breuer <m.breuer@consulting1x1.com>
2022-09-09 10:26:47 +0200

    removed duplicate column

--- database/migrations/02_create_analysis_table.php
+++ database/migrations/02_create_analysis_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string("symbol");
             $table->string("indicator");
             $table->string("value");
-            $table->string("value");
         });
     }
 
